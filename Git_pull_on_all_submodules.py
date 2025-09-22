e529f40e754efd1982ce2ae5e159e024776a449d
Yuanhe Li <liyuanhecpy@gmail.com>
2025-09-22 10:48:17 -0400

    Update Git_pull_on_all_submodules.py

--- Git_pull_on_all_submodules.py
+++ Git_pull_on_all_submodules.py
@@ -14,21 +14,25 @@ if __name__ == '__main__':
     MAIN_FOLDER = r"E:\My_Program"
     BASE_LIB = r"E:\My_Program\Python_Lib"
     exclude_folders = ["E:\My_Program\Gaussian_Toolkit_Backup"]
-    for project in list_current_folder(MAIN_FOLDER):
-        if project in exclude_folders:
-            continue
-        sub_repo_folder = os.path.join(project,'Python_Lib')
-        if os.path.isdir(sub_repo_folder):
-            os.chdir(sub_repo_folder)
-            try:
-                _ = git.Repo(".").git_dir
-            except git.exc.InvalidGitRepositoryError:
+    project_holders = [MAIN_FOLDER,
+                      os.path.join(MAIN_FOLDER,"0_Machine_Learning")]
+
+    for main_folder in project_holders:
+        for project in list_current_folder(main_folder):
+            if project in exclude_folders:
                 continue
-            print(sub_repo_folder)
-            print("\n>>>git stash\n")
-            subprocess.call('git stash')
-            print("\n>>>git pull\n")
-            subprocess.call('git pull')
-            print("\n>>>git stash pop\n")
-            subprocess.call('git stash pop')
-            print("-------------Pull finished------------\n\n\n\n")
+            sub_repo_folder = os.path.join(project,'Python_Lib')
+            if os.path.isdir(sub_repo_folder):
+                os.chdir(sub_repo_folder)
+                try:
+                    _ = git.Repo(".").git_dir
+                except git.exc.InvalidGitRepositoryError:
+                    continue
+                print(sub_repo_folder)
+                print("\n>>>git stash\n")
+                subprocess.call('git stash')
+                print("\n>>>git pull\n")
+                subprocess.call('git pull')
+                print("\n>>>git stash pop\n")
+                subprocess.call('git stash pop')
+                print("-------------Pull finished------------\n\n\n\n")
